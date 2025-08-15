74e1d4059b69acbfdfc2ea043c6d5eb8410b4d30
Menu Temesgen <menutemesgen@gmail.com>
2025-08-16 01:04:04 +0300

    - implementation of models -- i added the serializer and views for the owner and admins including url paths -- i added the serializers for the hotel app.

--- app/backend/guzomate/accounts/views.py
+++ app/backend/guzomate/accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .permissions import IsAdmin, IsOwner, IsGuest, IsManagerOfHotel, IsReceptionist, IsOwnerofHotel
-from .serializers import StaffSerializer, GuestSerializer
+from .serializers import StaffSerializer, GuestSerializer, OwnerAdminSerializer
 from .models import User
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.exceptions import PermissionDenied
+from rest_framework import viewsets 
 # Create your views here.
 
 class GuestRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
@@ -12,7 +12,7 @@ class GuestRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated, IsOwner | IsAdmin]
     lookup_field = 'id'
     
-    
+
     def get_queryset(self):
         return User.objects.all()
 
@@ -39,20 +39,19 @@ class GuestListView(generics.ListAPIView):
 ## Staff users
 
 class StaffRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsOwner | IsManagerOfHotel | IsOwnerofHotel]
+    permission_classes = [IsManagerOfHotel | IsOwnerofHotel]
     serializer_class = StaffSerializer
     lookup_field = 'id'
 
     def get_queryset(self):
         return User.objects.filter(role__in=['Manager', 'Receptionist'])
         
-def perform_update(self, serializer):
-    serializer.save()
+    def perform_update(self, serializer):
+        serializer.save()
 
-def perform_destroy(self, instance):
-    instance.delete()
+    def perform_destroy(self, instance):
+        instance.delete()
 
-    
 class StaffCreateView(generics.CreateAPIView):
     serializer_class = StaffSerializer
     permission_classes = [IsManagerOfHotel | IsOwnerofHotel]
@@ -67,6 +66,55 @@ class StaffListView(generics.ListAPIView):
 
     def get_queryset(self):
         return User.objects.filter(role__in=['Manager', 'Receptionist'])
+
+## owner
+
+class OwnerRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = OwnerAdminSerializer
+    permission_classes = [IsAdmin]
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return User.objects.filter(role='Owner')
+    
+    def perform_update(self, serializer):
+        serializer.save()
+    
+    def perform_destroy(self, instance):
+        instance.delete()
+    
+class OwnerCreateView(generics.CreateAPIView):
+    serializer_class = OwnerAdminSerializer
+    permission_classes = [IsAdmin]
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class OwnerListView(generics.ListAPIView):
+    serializer_class = OwnerAdminSerializer
+    permission_classes = [IsAdmin]
+
+    def get_queryset(self):
+        return User.objects.filter(role='Owner')
+    
+## Admin
+
+class AdminViewSets(viewsets.ModelViewSet):
+    serializer_class = OwnerAdminSerializer
+    permission_classes = [IsAdmin]
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return User.objects.filter(role='Admin')
+
+    def perform_create(self, serializer):
+        
+        serializer.save()
+    
+    def perform_destroy(self, instance):
+        instance.delete()
+    
+
 
 
 # Create your views here.
--- app/backend/guzomate/hotel/serializers.py
+++ app/backend/guzomate/hotel/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Location, Image, City, Hotel, Room, Amenities, LocalAttraction, HotelCities, HotelAttraction, Event
 from accounts.utils.validators import validate_picture
 from django.db import transaction
+from accounts.models import User
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -48,7 +49,7 @@ class HotelImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = ['id', 'image', 'imageable_type', 'imageable_id', 'hotel_name', 'uploaded_at', 'hotel']
-        read_only_field = ['id', 'uploaded_at']
+        read_only_fields = ['id', 'uploaded_at']
     
     @transaction.atomic
     def create(self, validated_data):
@@ -75,7 +76,7 @@ class CityImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = ['id', 'image', 'imageable_type', 'imageable_id', 'city_name', 'uploaded_at', 'city']
-        read_only_field = ['id', 'uploaded_at']
+        read_only_fields = ['id', 'uploaded_at']
     
     @transaction.atomic
     def create(self, validated_data):
@@ -123,3 +124,277 @@ class CitySerializer(serializers.ModelSerializer):
             setattr(instance, attr, value)
         instance.save()
         return instance
+    
+class HotelSerializer(serializers.ModelSerializer):
+    location = LocationSerializer()
+    class Meta:
+        model = Hotel
+        fields = ['id', 'owner', 'name', 'location', 'star']
+        read_only_fields = ['id']
+    
+    def validate_owner(self, value):
+        try:
+            owner = User.objects.get(id=value)
+        except User.DoesNotExist:
+            raise serializers.ValidationError({"owner": "the user you entered is not an owner or doesn't exist."})
+        return owner
+    
+    @transaction.atomic
+    def create(self, validated_data):
+        location_data = validated_data.pop('location')
+        location = Location.objects.create(**location_data)
+        hotel = Hotel.objects.create(location=location, **validated_data)
+        return hotel
+    
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        location_data = validated_data.pop('location', None)
+        if location_data:
+            for attr, value in location_data.items():
+                setattr(instance.location, attr, value)
+            instance.location.save()
+        
+        owner = validated_data.pop(owner)
+        if owner:
+            instance.owner = owner
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+    
+class RoomSerializer(serializers.ModelSerializer):
+    hotel = serializers.UUIDField(write_only=True)
+    class Meta:
+        model = Room
+        fields = ['id', 'hotel', 'description', 'type', 'room_no', 'price_per_night', 'available', 'number_of_beds']
+        read_only_fields = ['id']
+
+    def validate_hotel(self, value):
+        try:
+            hotel = Hotel.objects.get(id=value)
+        except Hotel.DoesNotExist:
+            raise serializers.ValidationError({"Hotel": "the hotel you entered doesn't exist."})
+        return hotel
+    
+    @transaction.atomic
+    def create(self, validated_data):
+        hotel = validated_data.pop('hotel')
+        return Room.objects.create(hotel=hotel, **validated_data)
+   
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        if 'hotel' in validated_data:
+            hotel = validated_data.pop('hotel')
+            instance.hotel = hotel
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+class HotelAmenitiesSerializer(serializers.ModelSerializer):
+    hotel = serializers.UUIDField(write_only=True)
+    model = Amenities
+    class Meta:
+        fields = ['id', 'name', 'description', 'available', 'hotel', 'amenityable_type']
+        read_only_fields = ['id']
+
+    def validate_hotel(self, value):
+        try:
+            hotel = Hotel.objects.get(id=value)
+        except Hotel.DoesNotExist:
+            raise serializers.ValidationError({"Hotel": "the hotel you entered doesn't exist."})
+        return hotel
+    
+    @transaction.atomic
+    def create(self, validated_data):
+        hotel = validated_data.pop('hotel')
+        return Room.objects.create(hotel=hotel, **validated_data)
+   
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+    
+class RoomAmenitiesSerializer(serializers.ModelSerializer):
+    hotel = serializers.UUIDField(write_only=True)
+    room = serializers.UUIDField(write_only=True)
+    class Meta:
+        model = Amenities
+        fields = ['id', 'name', 'description', 'available', 'hotel', 'amenityable_type', 'amenityable_id']
+        read_only_fields = ['id']
+   
+    def validate_hotel(self, value):
+        try:
+            hotel = Hotel.objects.get(id=value)
+        except Hotel.DoesNotExist:
+            raise serializers.ValidationError({"Hotel": "the hotel you entered doesn't exist."})
+        return hotel
+    
+    def validate_room(self, value):
+        try:
+            room = Room.objects.get(id=value)
+        except Room.DoesNotExist:
+            raise serializers.ValidationError({"Room": "the selected room doesn't exist."})
+        return room
+    
+    @transaction.atomic
+    def create(self, validated_data):
+        hotel = validated_data.pop('hotel')
+        room = validated_data.pop('room')
+        return Room.objects.create(hotel=hotel, room=room, **validated_data)
+   
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+    
+class EventSerializer(serializers.ModelSerializer):
+    hotel = serializers.UUIDField(write_only=True)
+
+    class Meta:
+        model = Event
+        fields = ['id', 'title', 'description', 'accessibility', 'price', 'hotel']
+        read_only_fields = ['id']
+
+    def validate_hotel(self, value):
+        try:
+            hotel = Hotel.objects.get(id=value)
+        except Hotel.DoesNotExist:
+            raise serializers.ValidationError({"Hotel": "the hotel you entered doesn't exist."})
+        return hotel
+    @transaction.atomic
+    def create(self, validated_data):
+        hotel = validated_data.pop('hotel')
+        event = Event.objects.create(hotel=hotel, **validated_data)
+        return event
+    
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+    
+
+class LocalAttractionSerializer(serializers.ModelSerializer):
+    location = LocationSerializer()
+    city = serializers.UUIDField(write_only=True)
+
+    class Meta:
+        model = LocalAttraction
+        fields = ['id', 'name', 'description', 'accessibility', 'type', 'location', 'city', 'availability']
+        read_only_fields = ['id']
+    
+    def validate_city(self, value):
+        try:
+            city = City.objects.get(id=value)
+        except Hotel.DoesNotExist:
+            raise serializers.ValidationError({"City": "the City you entered doesn't exist."})
+        return city
+
+    @transaction.atomic
+    def create(self, validated_data):
+        location_data = validated_data.pop('location')
+        location = Location.objects.create(**location_data)
+        city = validated_data.pop('city')
+        attraction = LocalAttraction.objects.create(location=location, city=city, **validated_data)
+        return attraction
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        location_data = validated_data.pop('location', None)
+        city = validated_data.pop('city', None)
+        if location_data:
+            for attr, value in location_data.items():
+                setattr(instance.location, attr, value)
+            instance.location.save()
+        
+        if city:
+            instance.city=city
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+class HotelAttractionSerializer(serializers.ModelSerializer):
+    hotel = serializers.UUIDField(write_only=True)
+    attraction = serializers.UUIDField(write_only=True)
+
+    class Meta:
+        model = HotelAttraction
+        fields = ['id', 'hotel', 'attraction', 'distance']
+        read_only_field = ['id', 'distance']
+
+    def validate_hotel(self, value):
+        try:
+            hotel = Hotel.objects.get(id=value)
+        except Hotel.DoesNotExist:
+            raise serializers.ValidationError({"Hotel": "the hotel you entered doesn't exist."})
+        return hotel
+
+    def validate_attraction(self, value):
+        try: 
+            attraction = LocalAttraction.objects.get(id=value)
+        except LocalAttraction.DoesNotExist:
+             raise serializers.ValidationError({"Attraction": "the attraction you entered doesn't exist."})
+        return attraction
+    
+    @transaction.atomic
+    def create(self, validated_data):
+        hotel = validated_data.pop('hotel')
+        attraction = validated_data.pop('attraction')
+        hotelAttraction = HotelAttraction.objects.create(hotel=hotel, attraction=attraction, **validated_data)
+        return hotelAttraction
+    
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+    
+class HotelCitiesSerializers(serializers.ModelSerializer):
+    city = serializers.UUIDField(write_only=True)
+    hotel = serializers.UUIDField(write_only=True)
+
+    class Meta:
+        model = HotelCities
+        fields = ['id', 'city', 'hotel']
+        read_only_fields = ['id']
+
+
+    def validate_city(self, value):
+        try:
+            city = City.objects.get(id=value)
+        except Hotel.DoesNotExist:
+            raise serializers.ValidationError({"City": "the City you entered doesn't exist."})
+        return city
+    
+    def validate_hotel(self, value):
+        try:
+            hotel = Hotel.objects.get(id=value)
+        except Hotel.DoesNotExist:
+            raise serializers.ValidationError({"Hotel": "the hotel you entered doesn't exist."})
+        return hotel
+    
+    @transaction.atomic
+    def create(self, validated_data):
+        city = validated_data.pop('city')
+        hotel = validated_data.pop('hotel')
+        hotelCity = HotelCities.objects.create(city=city, hotel=hotel, **validated_data)
+        return hotelCity
+    
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+    
