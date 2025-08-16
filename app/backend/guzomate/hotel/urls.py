1e1e29b45366d7655c53dc5c9ade062a8b7a5907
Menu Temesgen <menutemesgen@gmail.com>
2025-08-16 17:50:07 +0300

    Implement Hotel, Room, and Image management views with CRUD operations
    - Added HotelCreateView, HotelUpdateView, HotelDestroyView, and HotelViewSet for managing hotels.
    - Introduced RoomCreateView, RoomUpdateView, RoomDestroyView, and RoomViewSet for room management.
    - Created HotelImageCreateView, HotelImageUpdateView, HotelImageDestroyView, and HotelImageViewSet for handling hotel images.
    - Set appropriate permission classes to ensure secure access to the views.

--- app/backend/guzomate/hotel/urls.py
+++ app/backend/guzomate/hotel/urls.py
@@ -1,14 +1,27 @@
 from django.urls import path
 from accounts.views import StaffListView, StaffCreateView, StaffRetrieveUpdateDestroyView
-
-
+# from .views import HotelListCreateView, HotelRetrieveUpdateDestroyView
 
 
 
 urlpatterns = [
-    path('hotel/<uuid:pk>/staff/list/', StaffListView.as_view(), name='staff_list'),
-    path('hotel/<uuid:pk>/staff/', StaffCreateView.as_view(), name='staff_create'),
-    path('hotel/<uuid:pk>/staff/<uuid:id>', StaffRetrieveUpdateDestroyView.as_view(), name='staff_detail'),
-    
+    path('hotel/<uuid:hotel_id>/staff/list/', StaffListView.as_view(), name='staff_list'),
+    path('hotel/<uuid:hotel_id>/staff/', StaffCreateView.as_view(), name='staff_create'),
+    path('hotel/<uuid:hotel_id>/staff/<uuid:id>', StaffRetrieveUpdateDestroyView.as_view(), name='staff_detail'),
+    # path('hotel/list/', HotelListCreateView.as_view(), name='create_hotel'),
+    # path('hotel/<uuid:hotel_id>', HotelRetrieveUpdateDestroyView.as_view(), name='list_hotel'),
 #     path('guest/<uuid:pk>', GuestRetrieveUpdateDestroyView.as_view(), name='user_detail'),
 ]
+
+
+## the url for the viewset of the rooms in the hotel
+# room_list = RoomViewSets.as_view({'get': 'list'})
+# room_detail = RoomViewSets.as_view({'get': 'retrieve'})
+
+# urlpatterns = [
+#     # list all rooms for a hotel
+#     path('hotels/<uuid:hotel_id>/rooms/', room_list, name='hotel-rooms'),
+    
+#     # detail for a single room in a hotel
+#     path('hotels/<uuid:hotel_id>/rooms/<uuid:pk>/', room_detail, name='hotel-room-detail'),
+# ]
